622bfad58d5b3325eb3e59e82158ea362d7c9585
Andreas Norman <andreas.norman@knowit.se>
2014-03-26 15:01:12 +0100

    Prevents the form from posting on "enter"

--- jquery.betterxforms.v1.0.js
+++ jquery.betterxforms.v1.0.js
@@ -12,27 +12,27 @@
  * For details please refer to: https://github.com/KnowitLabs/Bootstrap-3-XForms-Fix
  */
 
-(function($){
+(function ($) {
 
 	"use strict";
 
-	$.BetterXforms = function( el, options ) {
+	$.BetterXforms = function (el, options) {
 
 		// Base
 		var base = this;
-		
+
 		// Access to jQuery and DOM versions of element
-		base.$el = $( el );
+		base.$el = $(el);
 		base.el = el;
-		
+
 		// Add a reverse reference to the DOM object
-		base.$el.data( '.BetterXforms', base );
-		
+		base.$el.data('.BetterXforms', base);
+
 		// Init
-		base.init = function() {
+		base.init = function () {
 
 			// Get all the specified options
-			base.options = $.extend( {},$.BetterXforms.defaultOptions, options );
+			base.options = $.extend({}, $.BetterXforms.defaultOptions, options);
 
 			/**
 			 * TODO
@@ -45,65 +45,75 @@
 			var inputGroupClass = 'form-group';
 
 			// Loop through all the input elements
-			$( '.xform input, .xform select' ).each( function( index, e ) {
+			$('.xform input, .xform select').each(function (index, e) {
 
 				// Input element
 				var inputElement = this;
 
 				// Access to jQuery and DOM versions of element
-				inputElement.$el = $( inputElement );
+				inputElement.$el = $(inputElement);
 				inputElement.el = inputElement;
 
 				// Check element type
-				if( e.type === 'text' || e.type === 'select-one' || e.type === 'select-multiple' ) {
+				if (e.type === 'text' || e.type === 'select-one' || e.type === 'select-multiple') {
 
 					// The element is a text field or select element
 					// Add bootstrap class class
-					inputElement.$el.addClass( inputElementClass );
-				} else if( e.type === 'checkbox' || e.type === 'radio' ) {
+					inputElement.$el.addClass(inputElementClass);
+				} else if (e.type === 'checkbox' || e.type === 'radio') {
 
 					// The element is a checkbox or radio button
 					// Remove the group class and add element type class
-					inputElement.$el.closest( 'div' ).removeClass( inputGroupClass ).addClass( e.type );
+					inputElement.$el.closest('div').removeClass(inputGroupClass).addClass(e.type);
 				}
 			});
 
 			// Check if iCheck is enabled
-			if( base.options.iCheck.enabled ) {
+			if (base.options.iCheck.enabled) {
 
 				// Check if iCheck plugin is loaded
-				if( $.fn.iCheck ) {
+				if ($.fn.iCheck) {
 
 					// Run iCheck
-					base.iCheck( base, base.options.iCheck );
+					base.iCheck(base, base.options.iCheck);
 				};
 			}
 		};
-		
+
 		// iCheck
-		base.iCheck = function(base, iCheckOptions) {
+		base.iCheck = function (base, iCheckOptions) {
 
 			// Apply iCheck
-			$( base.el ).iCheck(iCheckOptions);
+			$(base.el).iCheck(iCheckOptions);
 		};
 
 		// Run initializer
 		base.init();
 	};
-	
+
 	// Default options
 	$.BetterXforms.defaultOptions = {
-		bsVersion : 3, // Bootstrap version
-		iCheck : {
-			enabled : false
+		bsVersion: 3, // Bootstrap version
+		iCheck: {
+			enabled: false
 		}
 	};
-	
+
 	// Prototype
-	$.fn.BetterXforms = function( options ) {
-		return this.each(function() {
-			( new $.BetterXforms( this, options ) );
+	$.fn.BetterXforms = function (options) {
+		return this.each(function () {
+			(new $.BetterXforms(this, options));
 		});
 	};
 
 })(jQuery);
+
+$(document).ready(function ($) {
+	// Prevents the form from posting on "enter"
+	$(".xform .form-control").keydown(function (e) {
+		if (e.keyCode == 13) {
+			e.preventDefault();
+			e.stopPropagation();
+		}
+	})
+});
